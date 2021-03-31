9baff381244eb5a2757a72c6a86a1a36bdd44976
Jack Lawson <ajacksified@gmail.com>
2021-03-31 08:42:16 -0400

    Remove package-lock from node packaging tests

--- .github/workflows/cd-packaging-tests/node/esmodule.mjs
+++ .github/workflows/cd-packaging-tests/node/esmodule.mjs
@@ -1,7 +1,7 @@
 // Verify that imports from the main export work:
-import { Notification } from "@inrupt/solid-client-notifications";
+import { WebsocketNotification as IndexWN } from "@inrupt/solid-client-notifications";
 // Verify that submodule imports work:
 import WebsocketNotification from "@inrupt/solid-client-notifications/websocketNotification";
 
-console.log(Notification);
+console.log(IndexWN);
 console.log(WebsocketNotification);
