3a5e00640fbe4fc661659d0ebd76df374efc5fd8
Nicolas Seydoux <nseydoux@inrupt.com>
2021-11-17 23:30:35 +0100

    Fix env vars

--- e2e/node/e2e.test.ts
+++ e2e/node/e2e.test.ts
@@ -19,7 +19,7 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-import { jest } from "@jest/globals";
+import { jest, describe, it, expect, afterEach } from "@jest/globals";
 import { Session } from "@inrupt/solid-client-authn-node";
 import { config } from "dotenv-flow";
 import * as openidClient from "openid-client";
