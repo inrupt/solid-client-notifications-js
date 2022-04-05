c03e50084396b09a1948a0435cab16959bb5d075
Zwifi <nseydoux@inrupt.com>
2022-04-05 15:17:00 +0200

    Add end-to-end test app (#229)
    This adds a simple end-to-end test app allowing the user to log in, connect a websocket and see the events stream. The app should be linked to the local code so that the built current library is under tests.

---
+++ e2e/browser/testApp/components/appContainer/index.tsx
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from "react";
+import {
+  login,
+  logout,
+  handleIncomingRedirect,
+  getDefaultSession,
+  Session,
+  ISessionInfo,
+} from "@inrupt/solid-client-authn-browser";
+import Notifications from "../notifications";
+
+const REDIRECT_URL = window.location.href;
+const APP_NAME = "Notifications browser-based tests app";
+const DEFAULT_ISSUER = "https://login.inrupt.com/";
+
+const NotificationContainer = ({
+  sessionInfo,
+}: {
+  sessionInfo?: ISessionInfo;
+}) => {
+  if (sessionInfo?.isLoggedIn) {
+    return <Notifications />;
+  } else {
+    return <></>;
+  }
+};
+
+export default function AppContainer() {
+  const [sessionInfo, setSessionInfo] = useState<ISessionInfo>();
+  const [issuer, setIssuer] = useState<string>(DEFAULT_ISSUER);
+
+  useEffect(() => {
+    handleIncomingRedirect().then(setSessionInfo);
+  }, []);
+
+  const handleLogin = async () => {
+    try {
+      // Login will redirect the user away so that they can log in the OIDC issuer,
+      // and back to the provided redirect URL (which should be controlled by your app).
+      await login({
+        redirectUrl: REDIRECT_URL,
+        oidcIssuer: issuer,
+        clientName: APP_NAME,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleLogout = async () => {
+    await logout();
+    setSessionInfo(undefined);
+  };
+
+  return (
+    <div>
+      <h1>{APP_NAME}</h1>
+      <p>
+        {sessionInfo?.isLoggedIn
+          ? `Logged in as ${sessionInfo.webId}`
+          : "Not logged in yet"}
+      </p>
+      <form>
+        <input
+          type="text"
+          value={issuer}
+          onChange={(e) => {
+            setIssuer(e.target.value);
+          }}
+        />
+        <button
+          onClick={async (e) => {
+            e.preventDefault();
+            await handleLogin();
+          }}
+        >
+          Log In
+        </button>
+        <button
+          onClick={async (e) => {
+            e.preventDefault();
+            await handleLogout();
+          }}
+        >
+          Log Out
+        </button>
+      </form>
+      <NotificationContainer sessionInfo={sessionInfo} />
+    </div>
+  );
+}
---
+++ e2e/browser/testApp/components/notifications/index.tsx
@@ -0,0 +1,148 @@
+import { WebsocketNotification } from "@inrupt/solid-client-notifications";
+import { useEffect, useState } from "react";
+import {
+  createContainerInContainer,
+  getSourceIri,
+  deleteContainer,
+  getPodUrlAll,
+} from "@inrupt/solid-client";
+import { getDefaultSession } from "@inrupt/solid-client-authn-browser";
+
+const session = getDefaultSession();
+
+const MessageList = (props: { messages: Array<any> }) => {
+  const { messages } = props;
+  return (
+    <ul data-testid="eventList">
+      {messages.map((message) => (
+        <li key={message.id}>
+          <pre>{JSON.stringify(message, null, 2)}</pre>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default function Notifications() {
+  const [socket, setSocket] = useState<WebsocketNotification>();
+  const [connectionStatus, setConnectionStatus] = useState<string>(
+    "disconnected"
+  );
+  const [parentContainerUrl, setParentContainerUrl] = useState<string>();
+  const [childContainerUrl, setChildContainerUrl] = useState<string>();
+  const [messageBus, setMessageBus] = useState<any[]>([]);
+
+  useEffect(() => {
+    if (session.info.webId !== undefined) {
+      getPodUrlAll(session.info.webId as string, {
+        fetch: session.fetch,
+      }).then((pods) => {
+        if (pods.length === 0) {
+          throw new Error("No pod root in webid profile");
+        }
+        setParentContainerUrl(pods[0]);
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (parentContainerUrl !== undefined && socket === undefined) {
+      setSocket(
+        new WebsocketNotification(parentContainerUrl, {
+          fetch: session.fetch,
+          gateway: "https://notification.inrupt.com",
+        })
+      );
+    }
+    if (socket !== undefined) {
+      socket.on("connected", () => setConnectionStatus("connected"));
+      socket.on("closed", () => {
+        setConnectionStatus("closed");
+        setMessageBus([]);
+      });
+      socket.on("error", () => setConnectionStatus("error"));
+      socket.on("message", (message) => {
+        setMessageBus((previousMessageBus) => [
+          JSON.parse(message),
+          ...previousMessageBus,
+        ]);
+      });
+    }
+  }, [socket, parentContainerUrl]);
+
+  return (
+    <div>
+      <p>
+        Websocket status:{" "}
+        <em>
+          <span data-testid="webSocketStatus">{connectionStatus}</span>
+        </em>
+      </p>
+      <p>
+        Child container:{" "}
+        <em>
+          <span data-testid="childContainerUrl">
+            {childContainerUrl ?? "None"}
+          </span>
+        </em>
+      </p>
+
+      <button
+        onClick={async (e) => {
+          e.preventDefault();
+          if (socket !== undefined) {
+            await socket.connect();
+          }
+        }}
+        data-testid="connectSocket"
+      >
+        Connect websocket
+      </button>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          if (socket !== undefined) {
+            socket.disconnect();
+          }
+        }}
+        data-testid="disconnectSocket"
+      >
+        Disconnect websocket
+      </button>
+      <br></br>
+      <button
+        onClick={async (e) => {
+          e.preventDefault();
+          if (parentContainerUrl !== undefined) {
+            setChildContainerUrl(
+              getSourceIri(
+                await createContainerInContainer(parentContainerUrl, {
+                  fetch: session.fetch,
+                })
+              )
+            );
+          }
+        }}
+        data-testid="createContainer"
+      >
+        Create container
+      </button>
+      <button
+        onClick={async (e) => {
+          e.preventDefault();
+          if (childContainerUrl !== undefined) {
+            deleteContainer(childContainerUrl, {
+              fetch: session.fetch,
+            });
+            setChildContainerUrl("None");
+          }
+        }}
+        data-testid="deleteContainer"
+      >
+        Delete container
+      </button>
+      <br />
+      <MessageList messages={messageBus} />
+    </div>
+  );
+}
