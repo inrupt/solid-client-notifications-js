986bb07318be3e8fff193b22acf2bfa07dbd5b53
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2022-08-03 10:48:11 +0200

    Chore: remove cd packaging (#341)
    * ci: remove continuous delivery workflows
    
    These were poorly implemented and resulted in us publishing thousands of versions of our packages
    
    * ci: simplify release workflow
    
    We don't actually need to use deployments here

--- .github/workflows/cd-packaging-tests/bundler-webpack/src/index.js
+++
@@ -1,7 +0,0 @@
-// Verify that imports from the main export work:
-import { WebsocketNotification as IndexWN } from "@inrupt/solid-client-notifications";
-// Verify that submodule imports work:
-import { WebsocketNotification } from "@inrupt/solid-client-notifications/websocketNotification";
-
-console.log(IndexWN);
-console.log(WebsocketNotification);
