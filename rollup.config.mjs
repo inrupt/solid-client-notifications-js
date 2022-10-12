6b50b5fbc9f13880520d928c07c14774e32d0674
Nicolas Seydoux <nseydoux@inrupt.com>
2022-10-12 10:31:19 +0200

    Fix rollup config for ESM compatibility

---
+++ rollup.config.mjs
@@ -0,0 +1,76 @@
+/**
+ * Copyright 2020 Inrupt Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+// The following is only possible from Node 18 onwards
+// import pkg from "./package.json" assert { type: "json" };
+
+// Until we only support Node 18+, this should be used instead
+// (see https://rollupjs.org/guide/en/#importing-packagejson) 
+import { createRequire } from 'node:module';
+const require = createRequire(import.meta.url);
+const pkg = require('./package.json');
+
+import typescript from "rollup-plugin-typescript2";
+
+export default {
+  input: "./src/index.ts",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+    },
+    {
+      file: pkg.module,
+      entryFileNames: "[name].es.js",
+      format: "esm",
+    },
+    {
+      dir: "dist",
+      entryFileNames: "[name].mjs",
+      format: "esm",
+      preserveModules: true,
+    },
+    {
+      dir: "umd",
+      format: "umd",
+      name: "SolidClient",
+    },
+  ],
+  plugins: [
+    typescript({
+      // Use our own version of TypeScript, rather than the one bundled with the plugin:
+      typescript: require("typescript"),
+      tsconfigOverride: {
+        compilerOptions: {
+          module: "esnext",
+        },
+        exclude: ["**/*.test.ts"],
+      },
+    }),
+  ],
+  external: [
+    "@inrupt/solid-client",
+    "@inrupt/solid-client-authn-browser",
+    "events",
+    "cross-fetch",
+    "isomorphic-ws",
+  ],
+};
