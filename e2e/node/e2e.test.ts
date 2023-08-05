bc809f76f37207fa87077d62546868224ce7fc63
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-08-05 23:24:30 +1000

    chore: import jest

--- e2e/node/e2e.test.ts
+++ e2e/node/e2e.test.ts
@@ -19,7 +19,7 @@
 // SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-import { describe, it, expect, afterEach, beforeEach } from "@jest/globals";
+import { describe, it, expect, afterEach, beforeEach, jest } from "@jest/globals";
 
 import type { Session } from "@inrupt/solid-client-authn-node";
 import {
