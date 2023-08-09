46da3914a8bf3d75b5c13c4fb27c4cc2e3ffc3d0
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-08-09 14:10:01 +1000

    chore: deprecate setSessionFetch

--- src/notification.ts
+++ src/notification.ts
@@ -133,7 +133,7 @@ export class BaseNotification {
    * @param sessionFetch
    * @deprecated In future versions of this library the fetch function MUST
    * be set in the constructor, and the fetch function will always be unathenticated
-   * if no fetch function is provided in the constructor.
+   * if that entry is left blank.
    */
   setSessionFetch = (sessionFetch: typeof crossFetch = crossFetch): void => {
     this.fetch = sessionFetch;
