e7e5bd829b3ce73b0267b61978feced47645631a
Nicolas Ayral Seydoux <nseydoux@inrupt.com>
2026-08-10 15:07:52 +0200

    Lint

--- src/notification.ts
+++ src/notification.ts
@@ -75,12 +75,13 @@ export class BaseNotification {
       return this.gateway;
     }
 
-    const wellKnown = await getWellKnownSolid(this.topic)
-      .catch((err: Error) => {
+    const wellKnown = await getWellKnownSolid(this.topic).catch(
+      (err: Error) => {
         // The storage server for the topic resource didn't respond well to
         // getWellKnownSolid requests:
         throw new NotSupported(err);
-      });
+      },
+    );
 
     const wellKnownSubjects = getThingAll(wellKnown, {
       acceptBlankNodes: true,
